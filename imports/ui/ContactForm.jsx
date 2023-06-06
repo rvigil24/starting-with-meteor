6eb4411ee597f259ed6d266f2ec8ea1917243e61
Ruben Vigil <rvigil@applaudostudios.com>
2023-06-06 00:55:43 -0600

    Add tailwind library

--- imports/ui/ContactForm.jsx
+++ imports/ui/ContactForm.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { ContactsCollection } from "../api/ContactsCollection";
+import { ErrorAlert } from "./components/ErrorAlert";
+import { SuccessAlert } from "./components/SuccessAlert";
 
 export const ContactForm = () => {
     const [input, setInput] = useState({
@@ -7,9 +9,26 @@ export const ContactForm = () => {
         email: "",
         imageUrl: "",
     });
+    const [error, setError] = React.useState("");
+    const [success, setSuccess] = React.useState("");
+
+    const showError = ({ message }) => {
+        setError(message);
+        setTimeout(() => {
+            setError("");
+        }, 5000);
+    }
+
+    const showSuccess = ({ message }) => {
+        setSuccess(message);
+        setTimeout(() => {
+            setSuccess("");
+        }, 5000);
+    }
 
     const saveContact = (e) => {
         e.preventDefault();
+        console.log("submitting");
         ContactsCollection.insert({ ...input }, (err, id) => {
             if (err) return console.error(err);
             setInput({
@@ -17,6 +36,7 @@ export const ContactForm = () => {
                 email: "",
                 imageUrl: ""
             });
+            showSuccess("User has been added successfully!");
         });
     };
 
@@ -25,44 +45,59 @@ export const ContactForm = () => {
         setInput({ ...input, [name]: value });
     }
 
-    return <form onSubmit={saveContact}>
-        {/* name */}
-        <div>
-            <label htmlFor="name">
-                Name{" "}
-            </label>
-            <input
-                onChange={onInputChange}
-                type="text"
-                name="name"
-                value={input["name"]}
-                id="name" />
+    return <form className="mt-6" onSubmit={saveContact}>
+        {error && <ErrorAlert message={error} />}
+        {success && <SuccessAlert message={success} />}
+        <div className="grid grid-cols-6 gap-6">
+            <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+                    Name
+                </label>
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={input.name}
+                    onChange={onInputChange}
+                    className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+            </div>
+
+            <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                    Email
+                </label>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={input.email}
+                    onChange={onInputChange}
+                    className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+            </div>
+
+            <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                <label htmlFor="imageUrl" className="block text-sm font-medium text-gray-700">
+                    Image URL
+                </label>
+                <input
+                    type="text"
+                    id="imageUrl"
+                    name="imageUrl"
+                    value={input.imageUrl}
+                    onChange={onInputChange}
+                    className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                />
+            </div>
         </div>
-        {/* email */}
-        <div>
-            <label htmlFor="email">
-                Email{" "}
-            </label>
-            <input
-                onChange={onInputChange}
-                type="email"
-                name="email"
-                value={input["email"]}
-                id="email" />
+        <div className="px-2 py-3 text-right">
+            <button
+                type="submit"
+                className="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
+            >
+                Save Contact
+            </button>
         </div>
-        {/* image url */}
-        <div>
-            <label htmlFor="imageUrl">
-                Image URL{" "}
-            </label>
-            <input
-                onChange={onInputChange}
-                type="text"
-                name="imageUrl"
-                value={input["imageUrl"]}
-                id="imageUrl" />
-        </div>
-        {/* save contact */}
-        <button>Save contact</button>
     </form>
 }
--- imports/ui/ContactList.jsx
+++ imports/ui/ContactList.jsx
@@ -6,17 +6,39 @@ import { ContactsCollection } from "../api/ContactsCollection";
 export const ContactList = () => {
     const contacts = useTracker(() => ContactsCollection.find({}).fetch());
 
-    return <ul>
-        <h2>Contact List</h2>
-        {
-            contacts.map((contact, index) => {
-                console.log(contact);
-                return (
-                    <li key={index}>
-                        {contact.name} - {contact.email}
-                    </li>
-                )
-            })
-        }
-    </ul>
+    return <div>
+        <div className="mt-10">
+            <h3 className="text-xs font-semibold text-gray-500 uppercase tracking-wide">
+                Contact List
+            </h3>
+            <ul role="list" className="mt-4 border-t border-b border-gray-200 divide-y divide-gray-200">
+                {
+                    contacts.map((contact, index) => {
+                        return (
+                            <li key={index} className="py-4 flex items-center justify-between space-x-3">
+                                <div className="min-w-0 flex-1 flex items-center space-x-3">
+                                    <div className="flex-shrink-0">
+                                        <img className="h-10 w-10 rounded-full" src={contact.imageUrl} alt="" />
+                                    </div>
+                                    <div className="min-w-0 flex-1">
+                                        <p className="text-sm font-medium text-gray-900 truncate">{contact.name}</p>
+                                        <p className="text-sm font-medium text-gray-500 truncate">{contact.email}</p>
+                                    </div>
+                                    <div>
+                                        {/* <a
+                                    href="#"
+                                    onClick={(event) => removeContact(event, contact._id)}
+                                    className="inline-flex items-center shadow-sm px-2.5 py-0.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50"
+                                >
+                                    Remove
+                                </a> */}
+                                    </div>
+                                </div>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
+    </div>
 }
